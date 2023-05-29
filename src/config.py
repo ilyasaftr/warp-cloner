cf9c6e67640d5b0b6dc23ceab037f85eacffb0ce
ilyasaftr <ilyasarecord@gmail.com>
2023-05-29 18:10:12 +0700

    add more keys and update output format

--- src/config.py
+++ src/config.py
@@ -6,7 +6,10 @@ class Settings(BaseSettings):
     BASE_KEYS: list[str] = Field(
         env='BASE_KEYS',
         default=[
-            '397Npr2h-e86Rw0H4-a38nzO62'
+            '397Npr2h-e86Rw0H4-a38nzO62', 'i6A0ch34-5H078ZeV-98NDAu51',
+            'PF812t6G-0ze64Sg7-53vH9G6g', 'K62C0Ui8-6o81hCY0-68ZQ3D4q',
+            '29EC3p6K-1G9I6Q8X-19IZE47a', 'il7Kd392-0T496IWt-c628r5lk',
+            'r32Q6gi5-b05sR9M1-b82czi13', 'HDB32N60-7Vy8zS19-h0g8T21l'
         ]
     )
     THREADS_COUNT: int = Field(env='THREADS_COUNT', default=1)
@@ -14,7 +17,9 @@ class Settings(BaseSettings):
     DEVICE_MODELS: list[str] = Field(env='DEVICE_MODELS', default=[])
     DELAY: int = Field(env='DELAY', default=25)
     OUTPUT_FILE: str = Field(env='OUTPUT_FILE', default='output.txt')
-    OUTPUT_FORMAT: str = Field(env='OUTPUT_FORMAT', default='{key} | {referral_count}')
+    OUTPUT_FORMAT: str = Field(
+        env='OUTPUT_FORMAT',
+        default='License Key : {key} | Bandwidth : {referral_count} GB')
 
     class Config:
         env_file = '.env'
