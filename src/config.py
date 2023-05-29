9f90b3ed93297c7a34c0206fe2e1ebb8aedb9b57
ilyasaftr <ilyasarecord@gmail.com>
2023-05-29 15:53:59 +0700

    Add Github Action

--- src/config.py
+++ src/config.py
@@ -6,12 +6,7 @@ class Settings(BaseSettings):
     BASE_KEYS: list[str] = Field(
         env='BASE_KEYS',
         default=[
-            '0uPt219x-58dCy16I-50hnp1R2',
-            'Rq781e4K-Jo0732qI-m0hD9r25',
-            'er5D2y30-1dG205le-5RN86AX7',
-            'M8zEo256-1Cw546av-QO0i371l',
-            '5QyI96g4-DLe7941P-1If49k5B',
-            '905URj6x-2I47b0cy-vDp2A839'
+            '397Npr2h-e86Rw0H4-a38nzO62'
         ]
     )
     THREADS_COUNT: int = Field(env='THREADS_COUNT', default=1)
